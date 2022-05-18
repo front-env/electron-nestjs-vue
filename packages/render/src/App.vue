90ce9b0e2c0984504e6e62abdd83844f93e605d8
王金强 <wangjinqiang@gongchang.com>
2022-05-18 10:58:47 +0800

    support element-plus

--- packages/render/src/App.vue
+++ packages/render/src/App.vue
@@ -20,6 +20,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      <el-button>Default</el-button>
     </div>
   </header>
 
