34daada1e009ba7600e8c51560d7638ba0da741e
王金强 <wangjinqiang@gongchang.com>
2022-05-20 08:36:05 +0800

    service port

--- packages/main/src/utils/get-port.ts
+++ packages/main/src/utils/get-port.ts
@@ -3,10 +3,9 @@ import { getPort as getServicePort } from 'get-port-please';
 let port = null;
 export const getPort = async () => {
   if (!port) {
-    console.log('=======');
     port = await getServicePort({
       port: 2999,
-      ports: [...new Array(50)].map((item, index) => 2900 + index),
+      portRange: [2900, 65535],
     });
   }
   console.log('port----: ', port);
