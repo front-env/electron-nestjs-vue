c5c612328b9b076052e8c44eabf6305847a9e2f8
王金强 <wangjinqiang@gongchang.com>
2022-05-30 13:40:03 +0800

    clean

--- packages/main/src/desktop/desktop.service.ts
+++ packages/main/src/desktop/desktop.service.ts
@@ -1,16 +1,13 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CommonService } from './service.common';
 import { IpcService } from './service.ipc';
 import { MainWindowService } from './service.main-window';
 
 @Injectable()
-export class DesktopService implements OnModuleInit {
+export class DesktopService {
   constructor(
     private readonly mainWindow: MainWindowService,
     private readonly ipcService: IpcService,
     private readonly commonService: CommonService,
   ) {}
-  async onModuleInit(): Promise<void> {
-    await this.commonService.prepare();
-  }
 }
