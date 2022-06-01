dc0a68f4bd63f6898ab3292730d9c87ca282be07
王金强 <wangjinqiang@gongchang.com>
2022-06-01 08:53:42 +0800

    clean

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
-import { useElectronStore } from "./stores/electron";
 import { RouterView } from "vue-router";
-
-const store = useElectronStore();
-store.initPreloadRoot();
 </script>
 
 <template>
-  <RouterView />
+  <RouterView :key="$route.path" />
 </template>
--- packages/renderer/src/electron.d.ts
+++ packages/renderer/src/electron.d.ts
@@ -8,6 +8,6 @@ export interface IElectronAPI {
 
 declare global {
   interface Window {
-    electronAPI?: IElectronAPI;
+    electronAPI: IElectronAPI;
   }
 }
