e96002970b733e95cf0f6447b19c47e0714c3cfe
王金强 <wangjinqiang@gongchang.com>
2022-06-02 11:22:01 +0800

    clean

--- packages/renderer/.eslintrc.js
+++ packages/renderer/.eslintrc.js
@@ -2,22 +2,21 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  parserOptions: {
-    project: ["tsconfig.eslint.json"],
-    tsconfigRootDir: __dirname,
-    sourceType: "module",
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
-  ],
-  ignorePatterns: [".eslintrc.js"],
   env: {
     "vue/setup-compiler-macros": true,
-    browser: true,
-    es6: true,
     node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "plugin:prettier/recommended",
+    "@vue/eslint-config-typescript/recommended",
+    "@vue/eslint-config-prettier",
+    ".eslintrc-auto-import.json",
+  ],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
